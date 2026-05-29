9dc20981cef076233ed02601c711e709b998dada
Alexey Ershov <alexa3mail@gmail.com>
2026-05-29 19:05:19 +0300

    remove redundant commented out code from package.nix

--- package.nix
+++ package.nix
@@ -10,12 +10,6 @@ rustPlatform.buildRustPackage {
   version = "0.1.0";
 
   src = ./.;
-  # cargoLock = {
-  #   lockFile = ./Cargo.lock;
-  #   outputHashes = {
-  #     "mcping-0.2.0" = "sha256-DzefFMeUF8l9tj+zrOEDnwOaTydMX2MBsnLIVuGQtm4=";
-  #   };
-  # };
   cargoHash = "sha256-qsTKSCdtC6jd9D41pblV0W/AZkwZ4Xq/vD4wptIMtUY=";
 
   SQLX_OFFLINE = "true";
